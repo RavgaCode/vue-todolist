f2ed2c6d03150654d82691e806dd156150248b11
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-06-27 15:52:50 +0200

    Milestone 1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 var app = new Vue({
   el: "#app",
   data: {
-    doList: [],
+    doList: [
+      {
+        text: "Fare la spesa",
+        done: true,
+      },
+      {
+        text: "Chiamare nonna",
+        done: false,
+      },
+      {
+        text: "Fissare appuntamento dentista",
+        done: false,
+      },
+    ],
   },
 });
