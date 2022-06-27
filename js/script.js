fbadc4547559116a31081bf590543fbc220c0dcc
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-06-27 16:33:38 +0200

    Milestone 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var app = new Vue({
   el: "#app",
   data: {
+    newTaskText: "",
+    newTask: "",
     doList: [
       {
         text: "Fare la spesa",
@@ -17,7 +19,14 @@ var app = new Vue({
     ],
   },
   methods: {
-    removeTodo(index) {
+    addNewTask() {
+      this.newTask = {
+        text: this.newTaskText,
+        done: false,
+      };
+      this.doList.push(this.newTask);
+    },
+    removeTask(index) {
       // Elimino l'elemento nell'array all'index specifico
       this.doList.splice(index, 1);
     },
